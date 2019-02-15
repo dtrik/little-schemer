f4e3f7c1158152a995a4d5d70c887bbaa4aced81
dtrik <damunano.1989@gmail.com>
2019-02-15 23:37:22 +0530

    pick from list

---
+++ LS_Functions/pick.scm
@@ -0,0 +1,10 @@
+#lang scheme
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
