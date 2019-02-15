d9ae386227ef007e86a83eaed5d77a5b8a16f265
dtrik <damunano.1989@gmail.com>
2019-02-15 23:40:18 +0530

    pick remainder

---
+++ LS_Functions/rempick.scm
@@ -0,0 +1,10 @@
+#lang scheme
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (cdr lat))
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
