767892791e17b0157e9b047023faff8d1a908559
dtrik <damunano.1989@gmail.com>
2019-02-15 23:18:16 +0530

    rewrite equality with greater/lesser

--- LS_Functions/equal.scm
+++ LS_Functions/equal.scm
@@ -1,7 +1,25 @@
 #lang scheme
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define greater?
+  (lambda (m n)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (greater? (sub1 m) (sub1 n))))))
+
+(define lesser?
+  (lambda (m n)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (lesser? (sub1 m) (sub1 n))))))
+
 (define equal?
   (lambda (m n)
     (cond
-      ((zero? m)(zero? n))
-      ((zero? n) #f)
-      (else (equal? (sub1 m) (sub1 n))))))
+      ((greater? m n) #f)
+      ((lesser? m n) #f)
+      (else #t))))
