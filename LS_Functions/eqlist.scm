371785c5a7d606f741d54a001c7edeb4eb309cb5
dtrik <damunano.1989@gmail.com>
2019-02-23 20:13:46 +0530

    completed functionality

--- LS_Functions/eqlist.scm
+++ LS_Functions/eqlist.scm
@@ -1,4 +1,9 @@
 #lang scheme
+
+(define atom?
+    (lambda (x)
+      (and (not (pair? x)) (not (null? x)))))
+
 (define sub1
   (lambda (n)
     (- n 1)))
@@ -34,9 +39,39 @@
 (define eqlist?
   (lambda (l1 l2)
     (cond
-      ((and (null? l1) (null? l2)) #t)
-      ((and (null? l1) (atom? (car l2))) #f)
-      ((null? l1) #f)
-      ((and (atom? car l1) (null? l2)) #f)
+      ((and (null? l1)
+            (null? l2))
+       #t)
+      ((and (null? l1)
+            (atom? (car l2)))
+       #f)
+      ((null? l1)
+       #f)
+      ((and (atom? (car l1))
+            (null? l2))
+       #f)
+      ((and (atom? (car l1))
+            (atom? (car l2)))
+       (and (equan? (car l1)
+                    (car l2))
+            (eqlist? (cdr l1)
+                     (cdr l2))))
+      ((atom? (car l1))
+       #f)
+      ((null? l2)
+       #f)
+      ((atom? (car l2))
+       #f)
+      (else
+       (and (eqlist? (car l1)
+                     (car l2))
+            (eqlist? (cdr l1)
+                     (cdr l2)))))))
+              
+              
+      
+     
+      
+      
       
       
