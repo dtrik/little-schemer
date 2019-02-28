dcd352fc42e6473b7bd70dde81e1d0bcf31fbd54
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-02-28 12:43:36 +0530

    Update greater.scm

--- LS_Functions/greater.scm
+++ LS_Functions/greater.scm
@@ -1,7 +1,10 @@
 #lang scheme
-(define sub1
-  (lambda (n)
-    (- n 1)))
+;function to check which num m is greater than num n 
+;using sub1
+;recursive call on greater? till m or n becomes zero
+;if m becomes zero first then false
+;if n becomes zero first then true
+;if both zero (equal), first cond is zero? m so false
 
 (define greater?
   (lambda (m n)
@@ -10,5 +13,6 @@
       ((zero? n) #t)
       (else (greater? (sub1 m) (sub1 n))))))
 
-
-               
+(define sub1
+  (lambda (n)
+    (- n 1)))
