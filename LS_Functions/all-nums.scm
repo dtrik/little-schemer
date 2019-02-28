8cbda8e56954832c1ed76e0cd418a7845eb30060
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-02-28 08:19:11 +0530

    Update all-nums.scm

--- LS_Functions/all-nums.scm
+++ LS_Functions/all-nums.scm
@@ -1,4 +1,9 @@
 #lang scheme
+;function to get all the numbers in a list of atoms
+;recursively call all-nums till lat is null
+;check first element of lat, if number, cons it to 
+;all-nums of rest of lat, else all-nums of rest of lat
+
 (define all-nums
   (lambda (lat)
     (cond
@@ -6,5 +11,4 @@
       (else
        (cond
          ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
-         (else (all-nums (cdr lat))))))))
-       
+         (else (all-nums (cdr lat))))))))    
