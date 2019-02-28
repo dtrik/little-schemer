fe42aa78617a5e2a203eb3e383299c17ce394b59
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-02-28 08:13:38 +0530

    Update add.scm
    adding explanation

--- LS_Functions/add.scm
+++ LS_Functions/add.scm
@@ -1,6 +1,19 @@
 #lang scheme
+;function to add two nums m and n
+;using add1 and sub1
+;recursive call on o+ till m is zero
+;at each call (m times) add1 to n
+
 (define o+
   (lambda (m n)
     (cond
       ((zero? m) n)
       (else (add1 (o+ n (sub1 m)))))))
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
