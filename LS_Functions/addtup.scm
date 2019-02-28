563a03ba7ca922c16725cace165966bcbfbaa8a0
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-02-28 08:17:01 +0530

    Update addtup.scm

--- LS_Functions/addtup.scm
+++ LS_Functions/addtup.scm
@@ -1,12 +1,26 @@
 #lang scheme
-(define o+
-  (lambda (m n)
-    (cond
-      ((zero? m) n)
-      (else (add1 (o+ n (sub1 m)))))))
+;function to add all numbers in a tup
+;using o+
+;recursively call addtup till tup is null
+;use o+ to add first element of tup to 
+;addtup of rest of tup
 
 (define addtup
   (lambda (tup)
     (cond
       ((null? tup) 0)
       (else (o+ (car tup) (addtup (cdr tup)))))))
+
+(define o+
+  (lambda (m n)
+    (cond
+      ((zero? m) n)
+      (else (add1 (o+ n (sub1 m)))))))
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
