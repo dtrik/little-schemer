3d13f87ea6c1781c381f602f7dc959790b923a13
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-02-28 17:03:08 +0530

    Update insertL.scm

--- LS_Functions/insertL.scm
+++ LS_Functions/insertL.scm
@@ -1,5 +1,9 @@
 #lang scheme
-;insert atom 'new' to the left of atom 'old' in list of atoms 'lat'
+;function to insert atom 'new' to the left of atom 'old' in list of atoms 'lat'
+;recursive call till lat is null
+;if not null, check if first of lat is old, then cons new with lat
+;else cons first of lat with recursion on rest of lat
+
 (define insertL
   (lambda (new old lat)
     (cond
