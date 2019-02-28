4ca8881b67b838872fe532689eade181750aa774
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-02-28 10:04:19 +0530

    Update firsts.scm

--- LS_Functions/firsts.scm
+++ LS_Functions/firsts.scm
@@ -1,5 +1,10 @@
 #lang scheme
 ;returns first S-expressions from 'l'
+;recursively call firsts till l is null
+;cons first element of first element of l
+;to firsts of l
+;eg: (firsts '((a b c) (d e f) (g h i))) = '(a d g)
+
 (define firsts
   (lambda (l)
     (cond
