35ca28146ed1e7b038442ff4c22be46bd3b83340
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-01 22:47:21 +0530

    Update is_atom.scm

--- LS_Functions/is_atom.scm
+++ LS_Functions/is_atom.scm
@@ -1,5 +1,7 @@
 #lang scheme
-;check if x is atom
+;function to check if x is atom
+;checks that x is not a pair (exactly two values) and not null
+
 (define atom?
     (lambda (x)
       (and (not (pair? x)) (not (null? x)))))
