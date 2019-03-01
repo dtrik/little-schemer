21bbf311b928a2abc5aca8ffa325f12849563ed2
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-01 22:49:29 +0530

    Update is_lat.scm

--- LS_Functions/is_lat.scm
+++ LS_Functions/is_lat.scm
@@ -1,5 +1,9 @@
 #lang scheme
-;check if 'l' is list of atoms
+;function to check if l is list of atoms
+;recurssion of lat? till l is null
+;if not null, check if first of l is atom, then check lat? of rest of l
+;else false
+
 (define lat?
   (lambda(l)
     (cond
