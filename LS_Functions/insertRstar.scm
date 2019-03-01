21c1399a3c611c68b408fae5b2859f37f02f221a
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-01 20:49:21 +0530

    Update insertRstar.scm

--- LS_Functions/insertRstar.scm
+++ LS_Functions/insertRstar.scm
@@ -1,10 +1,14 @@
 #lang scheme
-(define atom?
-    (lambda (x)
-      (and
-       (not (pair? x))
-       (not (null? x)))))
-    
+;function to insert atom new to right of atom old in lat l
+;using atom?
+;recursive call on insertR* till l is null
+;if l not null, check if first of l is atom
+;if atom, check if first of l is old
+;then cons old to cons of new to insertR* on rest of l
+;else, cons first of l to insertR* on rest of l
+;if not atom, then cons insertR* on first of l and 
+;insertR* on rest of l
+
 (define insertR*
   (lambda (new old l)
     (cond
@@ -23,4 +27,9 @@
                            (car l))
             (insertR* new old
                       (cdr l))))))
-          
+
+(define atom?
+    (lambda (x)
+      (and
+       (not (pair? x))
+       (not (null? x)))))
