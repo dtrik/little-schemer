f8b1cffa0c2574c4aa5b14f051537ed6060f26e4
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-02 17:53:48 +0530

    Update is_one.scm

--- LS_Functions/is_one.scm
+++ LS_Functions/is_one.scm
@@ -1,10 +1,14 @@
 #lang scheme
-(define sub1
-  (lambda (n)
-    (- n 1)))
+;check if number is 1
+;using sub1
+;subtract 1 from n once and check if 0
 
 (define one?
   (lambda (n)
     (cond
       ((zero? n) #f)
       (else (zero? sub1 n)))))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
