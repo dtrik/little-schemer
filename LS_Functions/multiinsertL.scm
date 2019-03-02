fb554ea40ff50f82e658bd60339cc776f313e4a7
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-02 18:06:49 +0530

    Update multiinsertL.scm

--- LS_Functions/multiinsertL.scm
+++ LS_Functions/multiinsertL.scm
@@ -1,5 +1,10 @@
 #lang scheme
-;;insert atom 'new' to left of all atom 'old' in list of atoms 'lat'
+;function to insert new to left of all old in lat
+;recursion on multinsertL till lat is null
+;if not null, check if first of lat is old
+;then cons new to cons of old to multiinsertL of rest of lat
+;else, cons first of lat to multiinsertL of rest of lat
+
 (define multiinsertL
   (lambda (new old lat)
     (cond
