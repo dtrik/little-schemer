f55a293ed8401c2f0ca7a711e69103afb4df6fde
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-02 17:59:06 +0530

    Update lesser.scm

--- LS_Functions/lesser.scm
+++ LS_Functions/lesser.scm
@@ -1,10 +1,18 @@
 #lang scheme
-(define sub1
-  (lambda (n)
-    (- n 1)))
+;function to check if m is lesser than n
+;using sub1
+;recursive call on lesser? till m or n becomes zero
+;if n becomes zero first then false
+;if m becomes zero first then true
+;if both zero (equal), first cond is zero? n so false
+
 (define lesser?
   (lambda (m n)
     (cond
       ((zero? n) #f)
       ((zero? m) #t)
       (else (lesser? (sub1 m) (sub1 n))))))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
