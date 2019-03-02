75c59f3254a49a48bbf0684044d1c07b1644bf0d
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-02 17:57:29 +0530

    Update length.scm

--- LS_Functions/length.scm
+++ LS_Functions/length.scm
@@ -1,10 +1,15 @@
 #lang scheme
-(define add1
-  (lambda (n)
-    (+ n 1)))
+;function to get length of list of atoms
+;using add1
+;recursion on length till lat is null
+;if not null, add1 to length of rest of lat
 
 (define length
   (lambda (lat)
     (cond
       ((null? lat) 0)
       (else (add1 (length (cdr lat)))))))
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
