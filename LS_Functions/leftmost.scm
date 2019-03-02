be2fed28dbe0aa7e6e22c8758d614934a3c483ae
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-02 17:56:06 +0530

    Update leftmost.scm

--- LS_Functions/leftmost.scm
+++ LS_Functions/leftmost.scm
@@ -1,10 +1,15 @@
 #lang scheme
-(define atom?
-    (lambda (x)
-      (and (not (pair? x)) (not (null? x)))))
+;function to get the leftmost atom of l
+;using atom?
+;recursion on leftmost till first of l is atom
+;if atom get first of l
 
 (define leftmost
   (lambda (l)
     (cond
       ((atom? (car l)) (car l))
       (else (leftmost (car l))))))
+
+(define atom?
+    (lambda (x)
+      (and (not (pair? x)) (not (null? x)))))
