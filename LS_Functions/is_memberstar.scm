04da596bd725956a990de44f8fd9a7747d66973a
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-02 17:51:28 +0530

    Update is_memberstar.scm

--- LS_Functions/is_memberstar.scm
+++ LS_Functions/is_memberstar.scm
@@ -1,7 +1,10 @@
 #lang scheme
-(define atom?
-    (lambda (x)
-      (and (not (pair? x)) (not (null? x)))))
+;function to check if atom is member of any of l
+;using atom?
+;recursion on member* till l is null
+;if not null, check if first of l is atom
+;then check if a is first of l or member* of rest of l
+;else check if a is member* of first of l or rest of l
 
 (define member*
   (lambda (a l)
@@ -11,4 +14,7 @@
        (or ((eq? (a (car l))) #t)
            (member* a (cdr l))))
       (else (or (member* a (car l)) (member* a (cdr l)))))))
-        
+
+(define atom?
+    (lambda (x)
+      (and (not (pair? x)) (not (null? x)))))   
