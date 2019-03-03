e71d6e3d67a3f8d5a587c7d07c43055a2f8cae2f
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-03 21:43:31 +0530

    Update no-nums.scm

--- LS_Functions/no-nums.scm
+++ LS_Functions/no-nums.scm
@@ -1,4 +1,10 @@
 #lang scheme
+;function to remove all nums from list of atoms
+;recursion on no-nums till lat is null
+;if not null, check if first of lat is number
+;then no-nums on rest of lat
+;else cons first of lat to no-nums on rest of lat
+
 (define no-nums
   (lambda (lat)
     (cond
