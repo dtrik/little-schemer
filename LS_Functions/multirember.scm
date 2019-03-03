1d3ba2a1a6755f6f1d66efeed2c2cae1f42e8436
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-03 21:39:51 +0530

    Update multirember.scm

--- LS_Functions/multirember.scm
+++ LS_Functions/multirember.scm
@@ -1,5 +1,9 @@
 #lang scheme
-;;function that returns list of atoms 'lat' without any atom 'a'
+;function that returns list of atoms lat without any atom a
+;recursion on multirember till lat is null
+;if not null, check if first of lat is a, then multirember on rest of lat
+;else cons first of lat with multirember on rest of lat
+
 (define multirember
   (lambda (a lat)
     (cond
