57ff5a73a0311d5ee2d30e6faff2b20edac1490d
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-04 10:29:09 +0530

    Update occurstar.scm

--- LS_Functions/occurstar.scm
+++ LS_Functions/occurstar.scm
@@ -1,22 +1,11 @@
 #lang scheme
-
-(define atom?
-    (lambda (x)
-      (and (not (pair? x)) (not (null? x)))))
-
-(define add1
-  (lambda (n)
-    (+ n 1)))
-
-(define sub1
-  (lambda (n)
-    (- n 1)))
-
-(define o+
-  (lambda (m n)
-    (cond
-      ((zero? m) n)
-      (else (add1 (o+ n (sub1 m)))))))
+;function to check number of atom a in list l
+;using o+, add1 and atom?
+;recursion on occur* till l is null
+;if not null, check if first of l is atom
+;then check if first of l is a, then add1 to occur* on rest of l
+;else occur* on rest of l
+;if first of l not atom o+ occur* on first of l to occur* on rest of l 
 
 (define occur*
   (lambda (a l)
@@ -29,5 +18,21 @@
          (else (occur* a (cdr l))))))
       (else
        (o+ (occur* a (car l)) (occur* a (cdr l)))))))
-          
-         
+
+(define o+
+  (lambda (m n)
+    (cond
+      ((zero? m) n)
+      (else (add1 (o+ n (sub1 m)))))))
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define atom?
+    (lambda (x)
+      (and (not (pair? x)) (not (null? x)))))
