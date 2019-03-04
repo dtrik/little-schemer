d2c4883802845e810c20bf952b107748618bf88e
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-04 10:25:32 +0530

    Update occur.scm

--- LS_Functions/occur.scm
+++ LS_Functions/occur.scm
@@ -1,7 +1,10 @@
 #lang scheme
-(define add1
-  (lambda (n)
-    (+ n 1)))
+;function to check number of atom a in list of atoms lat
+;using add1
+;recursion on occur till lat is null
+;if not null, check if first of lat is a, then add1 to
+;occur on rest of lat
+;else occur on rest of lat
 
 (define occur
   (lambda (a lat)
@@ -11,4 +14,7 @@
        (cond
          ((eq? a (car lat) (add1 (occur a (cdr lat)))))
          (else (occur a (cdr lat))))))))
-        
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
