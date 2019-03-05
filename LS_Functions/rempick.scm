6a59b9c9a168a7d825a5e0d7b233fd4e3485facc
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:09:12 +0530

    Update rempick.scm

--- LS_Functions/rempick.scm
+++ LS_Functions/rempick.scm
@@ -1,10 +1,16 @@
 #lang scheme
-(define sub1
-  (lambda (n)
-    (- n 1)))
+;function to remove nth atom from list of atoms lat
+;using sub1
+;recursion on rempick till n is 1
+;if n 1, return rest of lat
+;else cons first of lat to rempick n-1 on rest of lat
 
 (define rempick
   (lambda (n lat)
     (cond
       ((zero? (sub1 n)) (cdr lat))
       (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
