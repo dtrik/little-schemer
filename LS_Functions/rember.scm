6998e80e8ff135b022d82b3379ac830d65ace9de
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:05:02 +0530

    Update rember.scm

--- LS_Functions/rember.scm
+++ LS_Functions/rember.scm
@@ -1,5 +1,10 @@
 #lang scheme
-;remove atom 'a' from list of atoms 'lat'
+;function to remove atom a from list of atoms lat
+;recursion on rember till lat is null
+;if not null, check if first of lat is a
+;then rest of lat
+;else cons first of lat to rember of rest of lat
+
 (define rember
   (lambda (a lat)
     (cond
