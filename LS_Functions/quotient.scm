4ee7ac52a34ebc74256296b3e95496987b344004
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:03:07 +0530

    Update quotient.scm

--- LS_Functions/quotient.scm
+++ LS_Functions/quotient.scm
@@ -1,7 +1,15 @@
 #lang scheme
-(define sub1
-  (lambda (n)
-    (- n 1)))
+;function to divide m by n and return quotient
+;using o-
+;recursion on quotient till m is lesser than n
+;if not lesser, subtract n from m and add 1 to quotient
+;else 0
+
+(define quotient
+  (lambda (m n)
+    (cond
+      ((lesser? m n) 0)
+      (else (add1 (quotient (o- m n) n))))))
 
 (define lesser?
   (lambda (m n)
@@ -16,8 +24,6 @@
       ((zero? n) m)
       (else (sub1 (o- m (sub1 n)))))))
 
-(define quotient
-  (lambda (m n)
-    (cond
-      ((lesser? m n) 0)
-      (else (add1 (quotient (o- m n) n))))))
+(define sub1
+  (lambda (n)
+    (- n 1)))
