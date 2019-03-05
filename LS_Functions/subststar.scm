68a1d32d02b29910f1c0ec123fac71cbe2e669d1
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:16:59 +0530

    Update subststar.scm

--- LS_Functions/subststar.scm
+++ LS_Functions/subststar.scm
@@ -1,8 +1,11 @@
 #lang scheme
-
-(define atom?
-    (lambda (x)
-      (and (not (pair? x)) (not (null? x)))))
+;function to substitute all atom old with atom new in list l
+;using atom?
+;recursion on subst* till l is null
+;if not null, check if first of l is atom
+;then check if first of l is old, then cons new to subst* on rest of l
+;else cons first of l to subst* on rest of l
+;if not atom, cons subst* on first of l to subst* on rest of l
 
 (define subst*
   (lambda (new old l)
@@ -17,4 +20,7 @@
                      (subst* new old (cdr l))))))
       (else (cons (subst* new old (car l))
                   (subst* new old (cdr l)))))))
-                 
+
+(define atom?
+    (lambda (x)
+      (and (not (pair? x)) (not (null? x)))))
