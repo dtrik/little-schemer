89fbec3fd493c2d3a6a2206bd0620872e1bff334
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:19:27 +0530

    Update tupplus.scm

--- LS_Functions/tupplus.scm
+++ LS_Functions/tupplus.scm
@@ -1,9 +1,9 @@
 #lang scheme
-(define o+
-  (lambda (m n)
-    (cond
-      ((zero? m) n)
-      (else (add1 (o+ n (sub1 m)))))))
+;function to add two tuples
+;using o+
+;recursion on tup+ till tup1 or tup2 is null
+;if both not null, cons addition of first of tup1 and first of tup2
+;to tup+ of rest of tup1 and rest of tup2
 
 (define tup+
   (lambda (tup1 tup2)
@@ -12,4 +12,17 @@
       ((null? tup2) tup1)
       (else (cons (o+ (car tup1) (car tup2))
                   (tup+ (cdr tup1) (cdr tup2)))))))
-                      
+
+(define o+
+  (lambda (m n)
+    (cond
+      ((zero? m) n)
+      (else (add1 (o+ n (sub1 m)))))))
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+(- n 1)))                  
