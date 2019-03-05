979f5752f3547e332217c43fe7a2341a7b537866
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:13:09 +0530

    Update subst.scm

--- LS_Functions/subst.scm
+++ LS_Functions/subst.scm
@@ -1,5 +1,9 @@
 #lang scheme
-;substitute first instance of atom 'old' with atom 'new' in list of atoms 'lat'
+;function to substitute first instance of atom old with atom new in list of atoms lat
+;recursion on subst till lat is null
+;if not null, check if first of lat is old, then cons new with rest of lat
+;else cons first of lat with subst of rest of lat
+
 (define subst
   (lambda (new old lat)
     (cond
