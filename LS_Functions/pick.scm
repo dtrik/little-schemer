069efb181c2d144d5efdf11002cbf6681ab82a77
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 22:59:23 +0530

    Update pick.scm

--- LS_Functions/pick.scm
+++ LS_Functions/pick.scm
@@ -1,10 +1,16 @@
 #lang scheme
-(define sub1
-  (lambda (n)
-    (- n 1)))
+;function to pick nth atom in list of atoms lat 
+;using sub1
+;recursion on pick till n is 1
+;if n is 1, then pick first of lat
+;else pick n-1 of rest of lat
 
 (define pick
   (lambda (n lat)
     (cond
       ((zero? (sub1 n)) (car lat))
       (else (pick (sub1 n) (cdr lat))))))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
