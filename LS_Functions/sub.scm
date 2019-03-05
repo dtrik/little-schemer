b37262ab530e2061285f656aded56f9e9febe603
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:11:22 +0530

    Update sub.scm

--- LS_Functions/sub.scm
+++ LS_Functions/sub.scm
@@ -1,6 +1,16 @@
 #lang scheme
+;function to subtract n from m
+;using sub1
+;recursion on o- till n is 0
+;if n 0, return m
+;else subtract 1 from m-(n-1)
+
 (define o-
   (lambda (m n)
     (cond
       ((zero? n) m)
       (else (sub1 (o- m (sub1 n)))))))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
