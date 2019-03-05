14a407bb35bc899126ef549135c00a55f297e88c
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:07:15 +0530

    Update remberstar.scm

--- LS_Functions/remberstar.scm
+++ LS_Functions/remberstar.scm
@@ -1,7 +1,11 @@
 #lang scheme
-(define atom?
-    (lambda (x)
-      (and (not (pair? x)) (not (null? x)))))
+;function to remove all atom a from list l
+;using atom?
+;recursion on rember* till l is null
+;if not null, check if first of l is atom
+;if atom, check if first of l is a, then rember* on rest of l
+;else cons first of l with rember* on rest of l
+;if not atom, cons rember* of first of l with rember* on rest of l
 
 (define rember*
   (lambda (a l)
@@ -13,4 +17,7 @@
          (else (cons (car l) (rember* (cdr l))))))
       (else (cons (rember* (a (car l))
                            (rember* a (cdr l))))))))
-         
+
+(define atom?
+    (lambda (x)
+      (and (not (pair? x)) (not (null? x)))))
