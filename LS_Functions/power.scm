3b02a13cd08c661d4ea152c11f4adb09f45f5744
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:03:54 +0530

    Update power.scm

--- LS_Functions/power.scm
+++ LS_Functions/power.scm
@@ -4,6 +4,18 @@
 ;recursion on ^ till n is 0
 ;if not 0, multiply m with (n-1)th power of m
 
+(define ^
+  (lambda (m n)
+    (cond
+      ((zero? n) 1)
+      (else (x m (^ m (sub1 n)))))))
+
+(define x
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else (o+ n (x n (sub1 m)))))))
+
 (define o+
   (lambda (m n)
     (cond
@@ -13,15 +25,3 @@
 (define sub1
   (lambda (n)
     (- n 1)))
-
-(define x
-  (lambda (n m)
-    (cond
-      ((zero? m) 0)
-      (else (o+ n (x n (sub1 m)))))))
-
-(define ^
-  (lambda (m n)
-    (cond
-      ((zero? n) 1)
-      (else (x m (^ m (sub1 n)))))))
