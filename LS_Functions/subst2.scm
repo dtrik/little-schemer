e2911e02b7ca81701bda2b9a0204d27c745e1dae
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-05 23:14:50 +0530

    Update subst2.scm

--- LS_Functions/subst2.scm
+++ LS_Functions/subst2.scm
@@ -1,5 +1,10 @@
 #lang scheme
-;substitute first instance of atom 'o1' or atom 'o2' with atom 'new' in list of atoms 'lat'
+;function to substitute first instance of atom o1 or atom o2 with atom new in list of atoms lat
+;recursion on subst2 lat is null
+;if not null, check if first of lat is o1, then cons new to rest of lat
+;else if first of lat is o2, then cons new to rest of lat
+;else cons first of lat to subst2 on rest of lat
+
 (define subst2
   (lambda (new o1 o2 lat)
     (cond
