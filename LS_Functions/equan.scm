081f3ae40d15e52dbacfd8b4204af2f48cfba557
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-06 14:24:04 +0530

    Update equan.scm

--- LS_Functions/equan.scm
+++ LS_Functions/equan.scm
@@ -1,13 +1,23 @@
 #lang scheme
 ;function to check if two atoms a1 and a2 are equal
-;using equan?
+;using equal?
 ;if both are numbers, equal? on a1 and a2
 ;if either is number, false
 ;else eq? on a1 and a2
 
-(define sub1
-  (lambda (n)
-    (- n 1)))
+(define equan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2)) (equal? a1 a2))
+      ((or (number? a1) (number? a2)) #f)
+      (else (eq? a1 a2)))))
+
+(define equal?
+  (lambda (m n)
+    (cond
+      ((greater? m n) #f)
+      ((lesser? m n) #f)
+      (else #t))))
 
 (define greater?
   (lambda (m n)
@@ -23,16 +33,6 @@
       ((zero? m) #t)
       (else (lesser? (sub1 m) (sub1 n))))))
 
-(define equal?
-  (lambda (m n)
-    (cond
-      ((greater? m n) #f)
-      ((lesser? m n) #f)
-      (else #t))))
-
-(define equan?
-  (lambda (a1 a2)
-    (cond
-      ((and (number? a1) (number? a2)) (equal? a1 a2))
-      ((or (number? a1) (number? a2)) #f)
-      (else (eq? a1 a2)))))
+(define sub1
+  (lambda (n)
+    (- n 1)))
