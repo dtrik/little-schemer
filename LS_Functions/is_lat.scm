50081358d373383b9438fd814882b0f0761de1cb
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-12 22:52:58 +0530

    update with example

--- LS_Functions/is_lat.scm
+++ LS_Functions/is_lat.scm
@@ -3,6 +3,8 @@
 ;recursion of lat? till l is null
 ;if not null, check if first of l is atom, then check lat? of rest of l
 ;else false
+;eg (lat? (1 2 3)) = #t
+;   (lat? (1 '(a b) 3))) = #f
 
 (define lat?
   (lambda(l)
