35ed3b4c84485a65725b7a95de99596466400779
Damodaran Trikkadeeri <damunano.1989@gmail.com>
2019-03-12 22:55:45 +0530

    update with example

--- LS_Functions/is_atom.scm
+++ LS_Functions/is_atom.scm
@@ -1,8 +1,8 @@
 #lang scheme
 ;function to check if x is atom
 ;checks that x is not a pair (exactly two values) and not null
-;eg (atom? 10) = #t
-;   (atom? '(a b)) = #f
+;eg:    (atom? 10) = #t
+;       (atom? '(a b)) = #f
 
 (define atom?
     (lambda (x)
